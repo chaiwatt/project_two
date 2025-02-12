c91ea4423438d96ad0d699f40a0c6423a7696e00
chaiwatt <chaiwat_thaweechan@yahoo.com>
2025-02-12 12:13:23 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Product;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    $products = Product::all();
+    // dd($products);
+    return view('welcome',[
+        'products' => $products
+    ]);
+});
